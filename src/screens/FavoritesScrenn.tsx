46ac92f9bfa57e8d3adc2b25a7f8f3399eb54a77
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-17 12:37:27 -0300

    feat: add components initial

---
+++ src/screens/FavoritesScrenn.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FavoritesScrenn() {
+  return (
+    <View>
+      <Text>FavoritesScrenn</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/screens/OrderHistiyScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function OrderHistiyScreen() {
+  return (
+    <View>
+      <Text>OrderHistiyScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
